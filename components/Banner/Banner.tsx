a675b560903bfac2613a4e45d2ced6ec3fdc4347
Ivar Troost <ivar@desagaz.com>
2020-09-12 11:29:00 +0200

    Improve phrasing banner

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -14,7 +14,7 @@ const Banner = (props: {}) => (
         en leerlingen staan dan ook te popelen om aan de slag te gaan met
         informatica, maar de vraag naar docenten is al jaren groter dan het
         aanbod. Wij willen in 2023 ruim 3000 leerlingen Informatica onderwijs
-        aan kunnen bieden - <strong>help jij mee?</strong>
+        aan kunnen bieden - <strong>doe jij mee?</strong>
       </p>
     </div>
   </section>
