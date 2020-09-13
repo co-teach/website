1f291bd448bd5bb7437fdc2a4eb3f142a531e0b6
Ivar Troost <ivar@desagaz.com>
2020-09-13 17:16:53 +0200

    Add ImageDivider component for underneath nav bar

---
+++ components/ImageDivider/ImageDivider.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./ImageDivider.module.scss";
+
+const ImageDivider = (props: {}) => <div className={classes.ImageDivider} />;
+
+export default ImageDivider;
--- pages/over/index.tsx
+++ pages/over/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import React from "react";
 import Header from "../../components/Header/Header";
+import ImageDivider from "../../components/ImageDivider/ImageDivider";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <title>Co-Teach Informatica</title>
       </Head>
       <Header />
+      <ImageDivider />
       <main>
         <h1>
           An vero, inquit, quisquam potest probare, quod perceptfum, quod.
