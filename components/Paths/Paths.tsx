f23192004b6efd8f89cc4d57985ab0c8045b1365
Ivar Troost <ivar@desagaz.com>
2020-09-13 14:14:44 +0200

    Add illustrations to Paths component
    Of school and business.

--- components/Paths/Paths.tsx
+++ components/Paths/Paths.tsx
@@ -5,37 +5,45 @@ import Link from "next/link";
 const Paths = (props: {}) => (
   <section id={classes.Paths}>
     <div id={classes.Professionals}>
-      <h2>Voor bedrijven en professionals met oog voor de toekomst</h2>
-      <ul>
-        <li>Enthusiasmeer leerlingen om in IT te werken</li>
-        <li>Gebruik je expertise om authentieke uitdagingen te ontwerpen</li>
-        <li>Bouw bekwaamheden op in lesgeven en communicatie</li>
-        <li>Lever een maatschappelijke bijdrage in digitale geletterheid</li>
-      </ul>
-      <p>
-        Als gastdocent ontvang je een <em>crash course</em>, doorlopende
-        professionele begeleiding en heb je de vrijheid om zelf projectonderwijs
-        vorm te geven.{" "}
-        <Link href="/voor-professionals">
-          <a>Meer informatie voor IT professional, managers en eigenaren.</a>
-        </Link>
-      </p>
+      <img src="/images/professionals_illustration.svg" aria-hidden />
+      <div className={classes.content}>
+        <h2>Voor bedrijven en professionals met oog voor de toekomst</h2>
+        <ul>
+          <li>Enthusiasmeer leerlingen om in IT te werken</li>
+          <li>Gebruik je expertise om authentieke uitdagingen te ontwerpen</li>
+          <li>Bouw bekwaamheden op in lesgeven en communicatie</li>
+          <li>Lever een maatschappelijke bijdrage in digitale geletterheid</li>
+        </ul>
+        <p>
+          Als gastdocent ontvang je een <em>crash course</em>, doorlopende
+          professionele begeleiding en heb je de vrijheid om zelf
+          projectonderwijs vorm te geven.{" "}
+          <Link href="/voor-professionals">
+            <a>Meer informatie voor IT professional, managers en eigenaren.</a>
+          </Link>
+        </p>
+      </div>
     </div>
     <div id={classes.Schools}>
-      <h2>Voor leerlingen die hun hart willen volgen</h2>
-      <ul>
-        <li>Volg informatica, van de eerste kennismaking tot het eindexamen</li>
-        <li>Leer van professionals en bouw je netwerk op</li>
-        <li>Werk aan projecten die nu spelen in het werkveld</li>
-      </ul>
-      <p>
-        Door het aanbieden van zowel een leerlijn als projectonderwijs geeft
-        Co-Teach Informatica scholen de kans om ondanks het leraartekort toch
-        dit belangrijke vak vorm te kunnen geven.{" "}
-        <Link href="/voor-scholen">
-          <a>Meer informatie voor leerlingen, leraren en schoolbestuurders</a>
-        </Link>
-      </p>
+      <img src="/images/schools_illustration.svg" aria-hidden />
+      <div className={classes.content}>
+        <h2>Voor leerlingen die hun hart willen volgen</h2>
+        <ul>
+          <li>
+            Volg informatica, van de eerste kennismaking tot het eindexamen
+          </li>
+          <li>Leer van professionals en bouw je netwerk op</li>
+          <li>Werk aan projecten die nu spelen in het werkveld</li>
+        </ul>
+        <p>
+          Door het aanbieden van zowel een leerlijn als projectonderwijs geeft
+          Co-Teach Informatica scholen de kans om ondanks het leraartekort toch
+          dit belangrijke vak vorm te kunnen geven.{" "}
+          <Link href="/voor-scholen">
+            <a>Meer informatie voor leerlingen, leraren en schoolbestuurders</a>
+          </Link>
+        </p>
+      </div>
     </div>
   </section>
 );
