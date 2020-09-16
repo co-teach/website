4ae730276991ced07c70f6f4835e074b6dcda343
Ivar Troost <ivar@desagaz.com>
2020-09-16 08:13:03 +0200

    Remove stray import

--- pages/voor-professionals/index.tsx
+++ pages/voor-professionals/index.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import Header from "../../components/Header/Header";
 import classes from "./voor-professionals.module.scss";
 import SignatureHeader from "../../components/SignatureHeader/SignatureHeader";
-import Divider from "../../components/Divider/ImageDivider";
 import Footer from "../../components/Footer/Footer";
 
 const Index = (props: {}) => (
