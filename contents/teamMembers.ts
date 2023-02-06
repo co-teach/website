6caa2dbf0a169ed42227746311db7e47d95ff41c
Andor Michels <andor.michels@redian.nl>
2023-02-06 15:18:33 +0100

    Add Liesbeth to the team

--- contents/teamMembers.ts
+++ contents/teamMembers.ts
@@ -164,6 +164,13 @@ export const teamMembers: { [id: string]: PersonModel } = {
     lastName: "Huijting",
     organisation: "Universiteit Utrecht",
     roles: ["Landelijk Secretaresse"],
-    personalPage: "https://www.linkedin.com/in/heather-huijting-2aa201166/ ",
+    personalPage: "https://www.linkedin.com/in/heather-huijting-2aa201166/",
+  },
+  liesbeth: {
+    firstName: "Liesbeth",
+    lastName: "Von Nordheim",
+    organisation: "Universiteit Utrecht",
+    roles: ["Accountmanager"],
+    personalPage: "https://www.linkedin.com/in/liesbethvonnordheim/",
   },
 };
