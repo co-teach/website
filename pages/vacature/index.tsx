7a234def3e37637c0c9f3c171d02272c9dceeac7
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-04-26 18:02:45 +0200

    vacature test

--- pages/vacature/index.tsx
+++ pages/vacature/index.tsx
@@ -32,19 +32,52 @@ const Home = () => {
              Herken jij jezelf in het voorgaande, dan is dit jouw baan.
           </p>
 
-          <h2>Hoe?</h2>
-          <img
-            src="/images/co-teach informatica programma.png"
-            alt="Co-teaching en een online leerlijn maken het eindexamen mogelijk."
-            style={{ margin: "20px 0px" }}
-            width="1165"
-            height="330"
-          />
+          <h2>Co-Teach Informatica</h2>
           
+          Middelbare scholen en de leerlingen willen heel graag aan de slag met informatica. Co-Teach 
+          Informatica is een oplossing voor middelbare scholen die door het stijgende lerarentekort geen 
+          vakdocent informatica kunnen vinden. Met ons landelijk programma kunnen scholen het vak 
+          informatica aanbieden aan bovenbouw havo/vwo leerlingen zonder bevoegd informatica 
+          vakdocent op de school. Door een combinatie van een online leerlijn en projectonderwijs door 
+          IT-experts uit het bedrijfsleven krijgen de leerlingen de kans het schoolvak informatica met een 
+          schoolexamen af te ronden. Lees meer op www.co-teach.nl
 
-          <h3>
-            <span>1</span>Kerndomeinen – Online Leerlijn
-          </h3>
+          <h2>Wat ga je doen?</h2>
+           
+          De leerlingen krijgen via een online leerlijn kennis aangereikt over programmeren, architectuur, 
+datastructuren, algoritmen, automaten en grammatica’s. In deze functie ben je beschikbaar 
+voor vragen van leerlingen over de inhoud en opdrachten. Soms zullen dit eenvoudige vragen 
+zijn, soms moet je de stof op meerdere manieren uitleggen om deze bij een leerlingen over te 
+laten komen. Dat vereist geduld, flexibiliteit en aanpassingsvermogen.
+Daarnaast beoordeel je door de leerlingen 
+ingeleverd werk aan de hand van vastgestelde 
+rubrics, zodat je aan elke leerling een goed 
+onderbouwde feedback kunt geven.
+Afhankelijk van het rooster van de leerlingen moet 
+je beschikbaar zijn voor het beantwoorden van 
+vragen. Dit (nakijk)werk kan goed vanuit huis 
+worden opgepakt. 
+Daarnaast kan je ingezet worden voor andere taken zoals: 
+          <ul>
+              <li>
+                Vakinhoudelijk meedenken over het lesmateriaal
+              </li>
+              <li>
+                Materiaal plaatsen op online leerlijn en 
+                Klassenmappen inrichten
+              </li>
+              <li>
+                Begeleiding op school tijdens de module 
+                programmeren.
+            </li>
+            <li>
+                Presentatie tijdens een conferentie
+            </li>
+            <li>
+                Beheer en onderhoud van de website 
+            </li>
+            
+            </ul>
 
           <p>
             Leerlingen kunnen onder begeleiding van de schooldocent, de leerlijn
