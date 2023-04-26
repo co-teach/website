6fbc373fe8da91674edc839508f0e5c56bd15e2e
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-04-26 18:29:42 +0200

    Update index.tsx

--- pages/vacature/index.tsx
+++ pages/vacature/index.tsx
@@ -23,17 +23,17 @@ const Home = () => {
             Als Co-Teach Informatica student-assistent kijk jij huiswerk na van middelbare schoolleerlingen, 
             het ondersteunen van havo/vwo leerlingen en denk je mee bij de ontwikkeling van lesmateriaal. 
             Hierbij wordt je ondersteunt door vakdidacticus Informatica van Co-Teach Informatica.
-            
+          </p><p>
             Voor deze functie moet je een gedegen kennis van informatica hebben, uitstekende 
             communicatieve vaardigheden en affiniteit met de doelgroep. Bovendien moet je in staat zijn 
             leerlingen de begeleiding te geven die zij nodig hebben. Daarvoor moet je goed kunnen 
             luisteren en zul je geduldig, adaptief en flexibel moeten zijn.
-            
+          </p><p>  
              Herken jij jezelf in het voorgaande, dan is dit jouw baan.
           </p>
 
           <h2>Co-Teach Informatica</h2>
-          
+          <p>
           Middelbare scholen en de leerlingen willen heel graag aan de slag met informatica. Co-Teach 
           Informatica is een oplossing voor middelbare scholen die door het stijgende lerarentekort geen 
           vakdocent informatica kunnen vinden. Met ons landelijk programma kunnen scholen het vak 
@@ -41,23 +41,19 @@ const Home = () => {
           vakdocent op de school. Door een combinatie van een online leerlijn en projectonderwijs door 
           IT-experts uit het bedrijfsleven krijgen de leerlingen de kans het schoolvak informatica met een 
           schoolexamen af te ronden. Lees meer op www.co-teach.nl
+          </p>
 
           <h2>Wat ga je doen?</h2>
            
           De leerlingen krijgen via een online leerlijn kennis aangereikt over programmeren, architectuur, 
-datastructuren, algoritmen, automaten en grammatica’s. In deze functie ben je beschikbaar 
-voor vragen van leerlingen over de inhoud en opdrachten. Soms zullen dit eenvoudige vragen 
-zijn, soms moet je de stof op meerdere manieren uitleggen om deze bij een leerlingen over te 
-laten komen. Dat vereist geduld, flexibiliteit en aanpassingsvermogen.
-Daarnaast beoordeel je door de leerlingen 
-ingeleverd werk aan de hand van vastgestelde 
-rubrics, zodat je aan elke leerling een goed 
-onderbouwde feedback kunt geven.
-Afhankelijk van het rooster van de leerlingen moet 
-je beschikbaar zijn voor het beantwoorden van 
-vragen. Dit (nakijk)werk kan goed vanuit huis 
-worden opgepakt. 
-Daarnaast kan je ingezet worden voor andere taken zoals: 
+          datastructuren, algoritmen, automaten en grammatica’s. In deze functie ben je beschikbaar 
+          voor vragen van leerlingen over de inhoud en opdrachten. Soms zullen dit eenvoudige vragen 
+          zijn, soms moet je de stof op meerdere manieren uitleggen om deze bij een leerlingen over te 
+          laten komen. Dat vereist geduld, flexibiliteit en aanpassingsvermogen.
+          Daarnaast beoordeel je door de leerlingen ingeleverd werk aan de hand van vastgestelde 
+          rubrics, zodat je aan elke leerling een goed onderbouwde feedback kunt geven. 
+          Afhankelijk van het rooster van de leerlingen moet je beschikbaar zijn voor het beantwoorden van vragen. 
+          Dit (nakijk)werk kan goed vanuit huis worden opgepakt. Daarnaast kan je ingezet worden voor andere taken zoals: 
           <ul>
               <li>
                 Vakinhoudelijk meedenken over het lesmateriaal
@@ -78,35 +74,6 @@ Daarnaast kan je ingezet worden voor andere taken zoals:
             </li>
             
             </ul>
-
-          <p>
-            Leerlingen kunnen onder begeleiding van de schooldocent, de leerlijn
-            via het digitaal platform zelfstandig volgen. Op het platform worden
-            de leerlingen{" "}
-            <strong>
-              stap voor stap door de lessen geleid met tekst, video en
-              afbeeldingen
-            </strong>
-            . Vakdidactici Informatica van de universiteit hebben een leerroute
-            ontwikkeld compleet met mijlpaalopdrachten en eindtoetsen. Het
-            nakijken van het leerlingenwerk en het beantwoorden van vragen
-            gebeurd door Co-Teach Informatica.
-          </p>
-          
-          <h3>
-            <span>2</span>Keuzedomeinen – Projectonderwijs Gastdocent
-          </h3>
-          <p>
-            Onze <strong>gastdocenten</strong> zijn{" "}
-            <strong>ICT-professionals uit het bedrijfsleven</strong>. Ze brengen
-            de werkvloer naar de klas. Ze delen hun kennis, vertellen een
-            persoonlijk verhaal en brengen inspiratie door praktijkvoorbeelden.
-            Op inspirerende wijze leren leerlingen over nieuwe technologieën,
-            trends en ontwikkelingen op het gebied van ICT. Leerlingen doen
-            praktische ervaring op door projectopdrachten uit te voeren. Het
-            bezoek van een gastdocent aan school is een ervaring van onschatbare
-            waarde.
-          </p>
         </article>
       </main>
       <Footer />
