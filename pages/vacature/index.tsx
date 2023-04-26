b6cb4c6ed8e68b009eb602fbfac471f1a5f69487
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-04-26 17:26:14 +0200

    vacature test

--- pages/vacature/index.tsx
+++ pages/vacature/index.tsx
@@ -1,94 +1,84 @@
-import React from "react";
 import Head from "next/head";
+import React from "react";
 import Header from "../../components/Header/Header";
-import classes from "./vacature.module.scss";
-import SignatureHeader from "../../components/SignatureHeader/SignatureHeader";
 import Footer from "../../components/Footer/Footer";
+import Link from "next/link";
+import SignatureHeader from "../../components/SignatureHeader/SignatureHeader";
+import classes from "./over.module.scss";
 import { menuStructure } from "../../contents/menuStructure";
 
-const Index = () => (
-  <>
-    <Head>
-      <title>Het vak informatica in de bovenbouw</title>
-    </Head>
-    <Header navData={menuStructure} />
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Vacature</title>
+      </Head>
+      <Header navData={menuStructure} />
+      <main className={classes.Over}>
+        <article className="restrictedWidth">
 
-    <main className={classes.main}>
-      <article className="restrictedWidth">
-        <section className="columns">
-          <SignatureHeader
-            title="Het vak informatica"
-            subtitle="In dit vak ben je een denker en een doener."
+          <h2>Student-assistent Informatica gezocht voor Co-Teach Informatica (6-10uur per week)</h2>
+          <p>
+            Ben jij UU student Informatica én wil je een interessante veelzijdige bijbaan naast jouw studie.
+            Als Co-Teach Informatica student-assistent kijk jij huiswerk na van middelbare schoolleerlingen, 
+            het ondersteunen van havo/vwo leerlingen en denk je mee bij de ontwikkeling van lesmateriaal. 
+            Hierbij wordt je ondersteunt door vakdidacticus Informatica van Co-Teach Informatica.
+            
+            Voor deze functie moet je een gedegen kennis van informatica hebben, uitstekende 
+            communicatieve vaardigheden en affiniteit met de doelgroep. Bovendien moet je in staat zijn 
+            leerlingen de begeleiding te geven die zij nodig hebben. Daarvoor moet je goed kunnen 
+            luisteren en zul je geduldig, adaptief en flexibel moeten zijn.
+            
+             Herken jij jezelf in het voorgaande, dan is dit jouw baan.
+          </p>
+
+          <h2>Hoe?</h2>
+          <img
+            src="/images/co-teach informatica programma.png"
+            alt="Co-teaching en een online leerlijn maken het eindexamen mogelijk."
+            style={{ margin: "20px 0px" }}
+            width="1165"
+            height="330"
           />
+          
+
+          <h3>
+            <span>1</span>Kerndomeinen – Online Leerlijn
+          </h3>
 
           <p>
-          Met zijn uiteenlopende onderwerpen biedt het vak informatica voor elk wat wils
-          en past wonderwel in elk profiel. De kennis en vaardigheden geven een voorsprong 
-          bij toekomstige studies en loopbaantrajecten, ongeacht of je verder wil met informatica.
+            Leerlingen kunnen onder begeleiding van de schooldocent, de leerlijn
+            via het digitaal platform zelfstandig volgen. Op het platform worden
+            de leerlingen{" "}
+            <strong>
+              stap voor stap door de lessen geleid met tekst, video en
+              afbeeldingen
+            </strong>
+            . Vakdidactici Informatica van de universiteit hebben een leerroute
+            ontwikkeld compleet met mijlpaalopdrachten en eindtoetsen. Het
+            nakijken van het leerlingenwerk en het beantwoorden van vragen
+            gebeurd door Co-Teach Informatica.
           </p>
           
-          
-          
-          <img
-            src={"/images/decorative_3.jpg"}
-            width="1552"
-            height="1030"
-            aria-hidden
-            alt={""}
-          />
-
-          <p className={classes.Lede}>
-            Van landbouw en gezondheidszorg tot onderwijs, logistiek en
-            wetenschap – in elke sector wordt met ICT gewerkt. Informatica staat
-            midden in onze maatschappij en onze informatietechnologie heeft
-            gevolgen voor jou en voor de maatschappij als geheel. Logisch toch
-            dat je wilt weten hoe alles werkt?
+          <h3>
+            <span>2</span>Keuzedomeinen – Projectonderwijs Gastdocent
+          </h3>
+          <p>
+            Onze <strong>gastdocenten</strong> zijn{" "}
+            <strong>ICT-professionals uit het bedrijfsleven</strong>. Ze brengen
+            de werkvloer naar de klas. Ze delen hun kennis, vertellen een
+            persoonlijk verhaal en brengen inspiratie door praktijkvoorbeelden.
+            Op inspirerende wijze leren leerlingen over nieuwe technologieën,
+            trends en ontwikkelingen op het gebied van ICT. Leerlingen doen
+            praktische ervaring op door projectopdrachten uit te voeren. Het
+            bezoek van een gastdocent aan school is een ervaring van onschatbare
+            waarde.
           </p>
-          
+        </article>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-          <div className={classes.BodyText}>
-            <p>
-              Bij informatica leer je belangrijke computer- en algemene
-              vaardigheden. Je leert bijvoorbeeld programmeren, zodat je je
-              eigen app of website kunt maken. Maar, informatica is veel meer
-              dan dat. Zo kan je onderzoeken hoe je de gebruiksvriendelijkheid
-              en bruikbaarheid (user experience en usability) van een product
-              kunt verbeteren. Je gaat problemen analyseren en oplossen door te
-              kijken naar algoritmen en automaten. Wat dat zijn, leer je bij
-              informatica.
-            </p>
-            <blockquote>
-              "Informatica is niet alleen een theoretisch vak, maar vooral een
-              praktisch vak waarbij je onderzoekt, bedenkt, maakt en doet"
-            </blockquote>
-            <p>
-              Je leert hoe computers en datanetwerken werken, hoe je veiliger
-              kunt internetten (security) en hoe je (grote hoeveelheden)
-              gegevens kunt verwerken (via databases of lerende algoritmes). Heb
-              je er wel eens over nagedacht dat via social media en het gebruik
-              van internet en apps veel gegevens over jou verzameld en
-              opgeslagen worden? Wat wordt daar mee gedaan? Dat zijn
-              maatschappelijke gevolgen waar we bij informatica ook naar kijken.
-            </p>
-
-            <p>
-              Informatica is niet alleen een theoretisch vak, maar vooral een
-              praktisch vak waarbij je onderzoekt, bedenkt, maakt en doet. Dat
-              doe je niet alleen, maar ook veel samen. Samenwerken is een
-              vaardigheid die je overal kunt toepassen, wat je keuzes in de
-              toekomst ook zijn.
-            </p>
-            <p>
-              Kortom, informatica is een actueel, boeiend en breed vak dat elke
-              leerling zou moeten kunnen volgen. Zelfs als er te weinig
-              informaticaleraren zijn.
-            </p>
-          </div>
-        </section>
-      </article>
-    </main>
-    <Footer />
-  </>
-);
-
-export default Index;
+export default Home;
