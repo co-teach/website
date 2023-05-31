d80c9ca2ab0bb29c8ff86074f0881ce18add9d41
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-05-31 19:31:39 +0200

    change path

--- contents/menuStructure.ts
+++ contents/menuStructure.ts
@@ -14,7 +14,7 @@ export const menuStructure: NavItem[] = [
     label: "Voor scholen",
   },
   {
-    href: "/voor-professionals",
+    href: "/voor-gastdocenten",
     label: "Voor gastdocenten",
   },
   { href: "/team", label: "Het team" },
