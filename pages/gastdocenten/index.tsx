537c336e77fa2ab03d5285b4049f8d6edfc74909
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-06-01 17:29:03 +0200

    Update index.tsx

--- pages/gastdocenten/index.tsx
+++ pages/gastdocenten/index.tsx
@@ -88,7 +88,7 @@ const Index = () => (
         <h2>Word gastdocent bij Co-Teach Informatica</h2>
         
         <img
-            src={"/images/Infographic_gastdocenten.png"}
+            src={"/images/Infographic_gastdocenten.pdf"}
             width="1552"
             height="1030"
             aria-hidden
