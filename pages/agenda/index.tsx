342732ac5330a5574f98780ed001a41444eedf63
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-06-07 23:30:46 +0200

    Update index.tsx

--- pages/agenda/index.tsx
+++ pages/agenda/index.tsx
@@ -31,15 +31,15 @@ const Home = () => {
               </li>
               <li>
                 13 juni 2023 | 16.15-17.00 uur 
-                <a href=https://events.teams.microsoft.com/event/e6b2051e-b802-48b3-8b97-4181e8d0438c@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7 
+                <a href="https://events.teams.microsoft.com/event/e6b2051e-b802-48b3-8b97-4181e8d0438c@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7">Microsoft Teams link</a>
               </li>
               <li>
                 15 juni 2023 | 16.15-17.00 uur 
-                https://events.teams.microsoft.com/event/beced50f-7f5f-4c5d-b287-6839440bada1@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7 
+                <a href="https://events.teams.microsoft.com/event/beced50f-7f5f-4c5d-b287-6839440bada1@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7">Microsoft Teams link</a>
                 </li>
               <li>
                 16 juni 2023 | 16.15-17.00 uur 
-                https://events.teams.microsoft.com/event/a6f9725d-9541-452d-8c4a-eb1fd93d90c7@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7 
+                <a href="https://events.teams.microsoft.com/event/a6f9725d-9541-452d-8c4a-eb1fd93d90c7@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7">Microsoft Teams link</a>
                 </li>
             </ul>
           </p>
