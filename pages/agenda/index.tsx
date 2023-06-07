caa26115e4001961c3313bf928a48f4fa7ab932b
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-06-07 23:06:27 +0200

    Update index.tsx

--- pages/agenda/index.tsx
+++ pages/agenda/index.tsx
@@ -33,11 +33,11 @@ const Home = () => {
                 13 juni 2023 | 16.15-17.00 uur 
                 https://events.teams.microsoft.com/event/e6b2051e-b802-48b3-8b97-4181e8d0438c@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7 
               </li>
-              <Li>
+              <li>
                 15 juni 2023 | 16.15-17.00 uur 
                 https://events.teams.microsoft.com/event/beced50f-7f5f-4c5d-b287-6839440bada1@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7 
                 </li>
-              <Li>
+              <li>
                 16 juni 2023 | 16.15-17.00 uur 
                 https://events.teams.microsoft.com/event/a6f9725d-9541-452d-8c4a-eb1fd93d90c7@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7 
                 </li>
