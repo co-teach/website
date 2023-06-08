ed0081a64c0997639fbf0846d3b69f74ea017e94
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-06-08 17:13:39 +0200

    Update Agenda

--- pages/agenda/index.tsx
+++ pages/agenda/index.tsx
@@ -19,8 +19,7 @@ const Home = () => {
           <SignatureHeader title="Agenda" subtitle="" />
 
           <h2>Aankomende bijeenkomsten, cursussen en events</h2>
-          <br />
-
+&nbsp;
           <h3>Webinars voor scholen</h3>
 
           <p>
@@ -29,26 +28,28 @@ const Home = () => {
 
           <ul>
             <li>
-              12 juni 2023 | 16.15-17.00 uur
-              <a href="https://events.teams.microsoft.com/event/6e72fddb-f04e-485d-9d9c-9cf2aee61d01@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7"> Microsoft Teams</a>
+              12 juni 2023 | 16.15-17.00 uur&nbsp;
+              <a href="https://events.teams.microsoft.com/event/6e72fddb-f04e-485d-9d9c-9cf2aee61d01@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7">Microsoft Teams</a>
             </li>
             <li>
-              13 juni 2023 | 16.15-17.00 uur
-              <a href="https://events.teams.microsoft.com/event/e6b2051e-b802-48b3-8b97-4181e8d0438c@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7"> Microsoft Teams</a>
+              13 juni 2023 | 16.15-17.00 uur&nbsp;
+              <a href="https://events.teams.microsoft.com/event/e6b2051e-b802-48b3-8b97-4181e8d0438c@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7">Microsoft Teams</a>
             </li>
             <li>
-              15 juni 2023 | 16.15-17.00 uur
-              <a href="https://events.teams.microsoft.com/event/beced50f-7f5f-4c5d-b287-6839440bada1@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7"> Microsoft Teams</a>
+              15 juni 2023 | 16.15-17.00 uur&nbsp;
+              <a href="https://events.teams.microsoft.com/event/beced50f-7f5f-4c5d-b287-6839440bada1@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7">Microsoft Teams</a>
             </li>
             <li>
-              16 juni 2023 | 16.15-17.00 uur
-              <a href="https://events.teams.microsoft.com/event/a6f9725d-9541-452d-8c4a-eb1fd93d90c7@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7"> Microsoft Teams</a>
+              16 juni 2023 | 16.15-17.00 uur&nbsp;
+              <a href="https://events.teams.microsoft.com/event/a6f9725d-9541-452d-8c4a-eb1fd93d90c7@d72758a0-a446-4e0f-a0aa-4bf95a4a10e7">Microsoft Teams</a>
             </li>
           </ul>
 
           <h3>Gastdocenten cursus planning</h3>
 
           <h2>GASTDOCENTEN CURSUS – UTRECHT - JUNI 2023</h2>
+          
+          Locatie: Universiteit Utrecht
 
           <ul>
             <li>
@@ -61,6 +62,37 @@ const Home = () => {
               Week 26: Vrijdag 30 Juni 2023 – Gastdocentencursus Deel III | Utrecht | 13u – 17u
             </li>
           </ul>
+          
+          <h2>GASTDOCENTEN CURSUS – DELFT - SEPTEMBER/OKTOBER 2023</h2>
+          
+          Locatie: TU Delft
+
+          <ul>
+            <li>
+              Week 37: Vrijdag 15 September 2023 – Gastdocentencursus Deel I | Delft | 14u – 17u
+            </li>
+            <li>
+              Week 38: Vrijdag 22 September 2023 – Gastdocentencursus Deel II | Online | 14u – 17u
+            </li>
+            <li>
+              Week 41: Vrijdag 13 Oktober 2023 – Gastdocentencursus Deel III | Delft | 13u – 17u
+            </li>
+          </ul>
+          
+          <h2>GASTDOCENTEN CURSUS – ENSCHEDE - OKTOBER 2023</h2>
+          
+          Locatie: Universiteit Twente
+
+          <ul>
+            <li>
+              Week 40: Vrijdag 6 Oktober 2023 – Gastdocentencursus Deel I & II | Enschede| 11u – 17u
+            </li>
+            <li>
+              Week 41: Vrijdag 13 Oktober 2023 – Gastdocentencursus Deel III | Enschede| 13u – 17u
+            </li>
+          </ul>
+          
+          
         </article>
       </main>
       <Footer />
