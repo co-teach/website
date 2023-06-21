8d5d5f32eb06e1a18ec460909b4881a05469c81b
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-06-21 13:25:12 +0200

    Update index.tsx

--- pages/gastdocenten/index.tsx
+++ pages/gastdocenten/index.tsx
@@ -164,8 +164,9 @@ const Index = () => (
             title="Aanmeldingsformulier"
             loading="lazy"
             className={classes.Survey}
-            src="https://utrechtuniversity.qualtrics.com/jfe/form/SV_eY9tyqz6pnMNw2x"
+            src="https://survey.uu.nl/jfe/form/SV_4VDqRnpY0svrMZE"
           />
+        </article>
         </section>
       </article>
     </main>
