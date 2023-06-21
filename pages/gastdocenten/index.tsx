43c5b13c595f3d5b372ae039ae3c3cb4ce5fe429
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-06-21 13:34:16 +0200

    Update index.tsx

--- pages/gastdocenten/index.tsx
+++ pages/gastdocenten/index.tsx
@@ -25,60 +25,49 @@ const Index = () => (
             subtitle="Breng de werkvloer naar de klas"
           />
 
-           <a
-  href={
-    "https://www.nldigital.nl/news/solution-architect-klaas-geeft-les-als-gastdocent/"
-  }
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <img
-    src={"/images/Klaas.png"}
-    alt={"Ervaringen van Klaas Heek"}
-  />
-</a>
+          <a
+            href={
+              "https://www.nldigital.nl/news/solution-architect-klaas-geeft-les-als-gastdocent/"
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={"/images/Klaas.png"}
+              alt={"Ervaringen van Klaas Heek"}
+            />
+          </a>
 
           <a
-  href={
-    "https://www.nldigital.nl/news/software-engineer-hanneke-staat-ook-voor-de-klas/"
-  }
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <img
-    src={"/images/Hanneke.png"}
-    alt={"Ervaringen van Hanneke den Ouden"}
-  />
-</a>
+            href={
+              "https://www.nldigital.nl/news/software-engineer-hanneke-staat-ook-voor-de-klas/"
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={"/images/Hanneke.png"}
+              alt={"Ervaringen van Hanneke den Ouden"}
+            />
+          </a>
 
           <p className={classes.Lede}>
-            Als IT-professionals uit het bedrijfsleven is het geven van
-            gastlessen aan leerlingen een ervaring van onschatbare waarde. Met
-            jouw vakexpertise breng je inspiratie met praktijkvoorbeelden. Doe
-            mee en draag je kennis over!
+            Als IT-professionals uit het bedrijfsleven is het geven van gastlessen aan leerlingen een ervaring van onschatbare waarde. Met jouw vakexpertise breng je inspiratie met praktijkvoorbeelden. Doe mee en draag je kennis over!
           </p>
 
           <div className={classes.BodyText}>
             <p>
-              We zoeken IT-professionals die gedurende 6 á 8 weken een
-              gastlessenreeks op school willen verzorgen.
+              We zoeken IT-professionals die gedurende 6 á 8 weken een gastlessenreeks op school willen verzorgen.
             </p>
             <blockquote>
-              “ALs gastdocent geef je les en begeleid leerlingen in een
-              informaticaproject”
+              “Als gastdocent geef je les en begeleid leerlingen in een informaticaproject”
             </blockquote>
             <p>
-              Je gaat niet onvoorbereid de klas in. We bieden je een
-              gastdocentencursus. Je geeft les onder begeleiding van een
-              vakdidacticus informatica en de schooldocent.
+              Je gaat niet onvoorbereid de klas in. We bieden je een gastdocentencursus. Je geeft les onder begeleiding van een vakdidacticus informatica en de schooldocent.
             </p>
 
             <p>
-              Ben je IT-expert, manager of werkgever en wil je een
-              maatschappelijke bijdrage leveren, dan zijn we op zoek naar jou.
-              Zet je inhoudelijke expertise in voor deze innovatieve vorm van
-              informatica-onderwijs en motiveer leerlingen om later
-              professioneel met IT aan de slag te gaan.
+              Ben je IT-expert, manager of werkgever en wil je een maatschappelijke bijdrage leveren, dan zijn we op zoek naar jou. Zet je inhoudelijke expertise in voor deze innovatieve vorm van informatica-onderwijs en motiveer leerlingen om later professioneel met IT aan de slag te gaan.
             </p>
           </div>
         </section>
@@ -86,18 +75,16 @@ const Index = () => (
         <hr />
 
         <h2>Word gastdocent bij Co-Teach Informatica</h2>
-        
+
         <img
-            src={"/images/Infographic_gastdocenten.png"}
-            width="1800"
-            height="1030"
-            aria-hidden
-            alt={""}
-          />
+          src={"/images/Infographic_gastdocenten.png"}
+          width="1800"
+          height="1030"
+          aria-hidden="true"
+          alt={""}
+        />
 
         <hr />
-        
-        
 
         <section>
           <details>
@@ -105,9 +92,8 @@ const Index = () => (
               <h2>Download hier onze gastdocenten brochure</h2>
             </summary>
             <p>
-             Wil je meer met je eigen praktijkervaring doen en voor de klas staan als gastdocent? 
+              Wil je meer met je eigen praktijkervaring doen en voor de klas staan als gastdocent?
               <a href="/docs/co-teach-informatica_brochure_gastdocenten.pdf">Download hier de Co-Teach Informatica gastdocent brochure.</a>
-              
             </p>
           </details>
         </section>
@@ -118,47 +104,37 @@ const Index = () => (
             </summary>
             <p>
               Dit zijn de aankomende gastdocenten cursus data.
-              
               <img
-            src={"/images/gastdocenten_cursus_planning.png"}
-            width="1241"
-            height="1755"
-            aria-hidden
-            alt={""}
-          />
+                src={"/images/gastdocenten_cursus_planning.png"}
+                width="1241"
+                height="1755"
+                aria-hidden="true"
+                alt={""}
+              />
             </p>
           </details>
         </section>
 
         <hr />
-        
+
         <section>
           <details>
             <summary>
               <h2>Vakexpertise</h2>
             </summary>
             <p>
-              In de bovenbouw klassen zijn we op zoek naar IT-experts die invulling
-          kunnen geven aan één van de lesdomeinen. Je kan{" "}
-          <a href="https://www.slo.nl/handreikingen/havo-vwo/handreiking-se-info-hv/examenprogramma/keuzedomeinen/">
-            hier
-          </a>{" "}
-          alle lesdomeinen vinden die gedurende de hele bovenbouw de revue
-          kunnen passeren. Uiteraard helpen we je met het kiezen van een gepast
-          traject en domein.
+              In de bovenbouw klassen zijn we op zoek naar IT-experts die invulling kunnen geven aan één van de lesdomeinen. Je kan{" "}
+              <a href="https://www.slo.nl/handreikingen/havo-vwo/handreiking-se-info-hv/examenprogramma/keuzedomeinen/">hier</a> alle lesdomeinen vinden die gedurende de hele bovenbouw de revue kunnen passeren. Uiteraard helpen we je met het kiezen van een gepast traject en domein.
             </p>
           </details>
         </section>
-        
 
         <hr />
 
         <section>
           <h2>Meld je nu aan</h2>
           <p>
-            Schrijf je hieronder in om kennis met ons te maken en verder te
-            kijken of Co-Teach Informatica iets voor jou is. We nemen contact
-            met je op.
+            Schrijf je hieronder in om kennis met ons te maken en verder te kijken of Co-Teach Informatica iets voor jou is. We nemen contact met je op.
           </p>
           <iframe
             title="Aanmeldingsformulier"
@@ -166,7 +142,6 @@ const Index = () => (
             className={classes.Survey}
             src="https://survey.uu.nl/jfe/form/SV_4VDqRnpY0svrMZE"
           />
-        </article>
         </section>
       </article>
     </main>
