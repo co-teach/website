2382a9c36a19c28a815f2bea607eb3d0d9d97d13
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-07-06 14:46:11 +0200

    re-direct gastdocenten page to new site

--- pages/gastdocenten/index.tsx
+++ pages/gastdocenten/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../../components/Header/Header";
 import classes from "./gastdocenten.module.scss";
 import SignatureHeader from "../../components/SignatureHeader/SignatureHeader";
@@ -8,145 +9,28 @@ import Link from "next/link";
 import { menuStructure } from "../../contents/menuStructure";
 import Video from "../../components/Video/Video";
 
-const Index = () => (
-  <>
-    <Head>
-      <title>
-        Co-Teach Informatica voor Professionals, Bedrijven en Organisaties
-      </title>
-    </Head>
-    <Header navData={menuStructure} />
+const Index = () => {
+  const router = useRouter();
 
-    <main className={classes.main}>
-      <article className="restrictedWidth">
-        <section className="columns">
-          <SignatureHeader
-            title="Draag bij aan meer informatica op scholen"
-            subtitle="Breng de werkvloer naar de klas"
-          />
+  useEffect(() => {
+    // Redirect to the desired URL
+    window.location.href = "https://co-teach.nl/gastdocenten/";
+  }, []);
 
-          <a
-            href={
-              "https://www.nldigital.nl/news/solution-architect-klaas-geeft-les-als-gastdocent/"
-            }
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={"/images/Klaas.png"}
-              alt={"Ervaringen van Klaas Heek"}
-            />
-          </a>
+  return (
+    <>
+      <Head>
+        <title>
+          Co-Teach Informatica voor Professionals, Bedrijven en Organisaties
+        </title>
+      </Head>
+      <Header navData={menuStructure} />
 
-          <a
-            href={
-              "https://www.nldigital.nl/news/software-engineer-hanneke-staat-ook-voor-de-klas/"
-            }
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={"/images/Hanneke.png"}
-              alt={"Ervaringen van Hanneke den Ouden"}
-            />
-          </a>
-
-          <p className={classes.Lede}>
-            Als IT-professionals uit het bedrijfsleven is het geven van gastlessen aan leerlingen een ervaring van onschatbare waarde. Met jouw vakexpertise breng je inspiratie met praktijkvoorbeelden. Doe mee en draag je kennis over!
-          </p>
-
-          <div className={classes.BodyText}>
-            <p>
-              We zoeken IT-professionals die gedurende 6 á 8 weken een gastlessenreeks op school willen verzorgen.
-            </p>
-            <blockquote>
-              “Als gastdocent geef je les en begeleid leerlingen in een informaticaproject”
-            </blockquote>
-            <p>
-              Je gaat niet onvoorbereid de klas in. We bieden je een gastdocentencursus. Je geeft les onder begeleiding van een vakdidacticus informatica en de schooldocent.
-            </p>
-
-            <p>
-              Ben je IT-expert, manager of werkgever en wil je een maatschappelijke bijdrage leveren, dan zijn we op zoek naar jou. Zet je inhoudelijke expertise in voor deze innovatieve vorm van informatica-onderwijs en motiveer leerlingen om later professioneel met IT aan de slag te gaan.
-            </p>
-          </div>
-        </section>
-
-        <hr />
-
-        <h2>Word gastdocent bij Co-Teach Informatica</h2>
-
-        <img
-          src={"/images/Infographic_gastdocenten.png"}
-          width="1800"
-          height="1030"
-          aria-hidden="true"
-          alt={""}
-        />
-
-        <hr />
-
-        <section>
-          <details>
-            <summary>
-              <h2>Download hier onze gastdocenten brochure</h2>
-            </summary>
-            <p>
-              Wil je meer met je eigen praktijkervaring doen en voor de klas staan als gastdocent?
-              <a href="/docs/co-teach-informatica_brochure_gastdocenten.pdf">Download hier de Co-Teach Informatica gastdocent brochure.</a>
-            </p>
-          </details>
-        </section>
-        <section>
-          <details>
-            <summary>
-              <h2>Gastdocenten cursus - 2023</h2>
-            </summary>
-            <p>
-              Dit zijn de aankomende gastdocenten cursus data.
-              <img
-                src={"/images/gastdocenten_cursus_planning.png"}
-                width="1241"
-                height="1755"
-                aria-hidden="true"
-                alt={""}
-              />
-            </p>
-          </details>
-        </section>
-
-        <hr />
-
-        <section>
-          <details>
-            <summary>
-              <h2>Vakexpertise</h2>
-            </summary>
-            <p>
-              In de bovenbouw klassen zijn we op zoek naar IT-experts die invulling kunnen geven aan één van de lesdomeinen. Je kan{" "}
-              <a href="https://www.slo.nl/handreikingen/havo-vwo/handreiking-se-info-hv/examenprogramma/keuzedomeinen/">hier</a> alle lesdomeinen vinden die gedurende de hele bovenbouw de revue kunnen passeren. Uiteraard helpen we je met het kiezen van een gepast traject en domein.
-            </p>
-          </details>
-        </section>
-
-        <hr />
-
-        <section>
-          <h2>Meld je nu aan</h2>
-          <p>
-            Schrijf je hieronder in om kennis met ons te maken en verder te kijken of Co-Teach Informatica iets voor jou is. We nemen contact met je op.
-          </p>
-          <iframe
-            title="Aanmeldingsformulier"
-            loading="lazy"
-            className={classes.Survey}
-            src="https://survey.uu.nl/jfe/form/SV_4VDqRnpY0svrMZE"
-          />
-        </section>
-      </article>
-    </main>
-    <Footer />
-  </>
-);
+      {/* Add your custom content here if needed */}
+      
+      <Footer />
+    </>
+  );
+};
 
 export default Index;
