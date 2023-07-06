6db68505908be5008410c15a1e94997065e68237
PaulinaaM <61416241+PaulinaaM@users.noreply.github.com>
2023-07-06 14:41:05 +0200

    redirect contact page to new site

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,9 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="refresh" content="0;url=https://co-teach.nl/contact/">
-<title>Redirecting...</title>
-</head>
-<body>
-</body>
-</html>
+import React, { useEffect } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Header from "../../components/Header/Header";
+import classes from "./gastdocenten.module.scss";
+import SignatureHeader from "../../components/SignatureHeader/SignatureHeader";
+import Footer from "../../components/Footer/Footer";
+import Link from "next/link";
+import { menuStructure } from "../../contents/menuStructure";
+import Video from "../../components/Video/Video";
+
+const Index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to the desired URL
+    window.location.href = "https://co-teach.nl/contact/";
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>
+          Co-Teach Informatica voor Professionals, Bedrijven en Organisaties
+        </title>
+      </Head>
+      <Header navData={menuStructure} />
+
+      {/* Add your custom content here if needed */}
+      
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
